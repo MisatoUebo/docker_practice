97add6c5d232b3c74abc3f8fc43059c4e0b56fdf
Misato Uebo <misato.uebo@tongullman.co.jp>
2022-08-23 13:55:57 +0900

    URLに値を渡してhtml内に表示できるように変更

--- view2.py
+++ view2.py
@@ -4,9 +4,9 @@ from flask_bootstrap import Bootstrap
 
 bp2 = Blueprint('view2', __name__, url_prefix='/prc')
 
-@bp2.route("/")
-def hello():
-    return render_template('hello.html')
+@bp2.route("/<parameter>")
+def hello(parameter):
+    return render_template('hello.html',parameter=parameter)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
