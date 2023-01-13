8520aa0ca362f851d2b277db45b79e276472246f
Misato Uebo <misato.uebo@tongullman.co.jp>
2023-01-14 01:53:24 +0900

    idが自動でつくように変更

--- app/src/models/todo.py
+++ app/src/models/todo.py
@@ -4,7 +4,7 @@ from database import db
 class Todo(db.Model):
   __tablename__ = 'todos'
 
-  id = db.Column(db.Integer, primary_key=True)
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   name = db.Column(db.String(255))
   #state = db.Column(db.String(255), nullable=False , primary_key=True)
 
