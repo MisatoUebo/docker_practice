4b95956aa644fcac61daef987ed3c6d8977e0d21
MisatoUebo <misato.uebo@tongull.co.jp>
2023-03-27 00:12:32 +0900

    データの追加・削除用のHTMLファイルを追加

--- app/src/app.py
+++ app/src/app.py
@@ -2,6 +2,7 @@
 from flask import Flask,render_template
 from database import init_db,db
 from models import Todo
+from flask import request
 
 def create_app():
     app = Flask(__name__)
@@ -23,20 +24,34 @@ def create_app():
 
     @app.route('/add')
     def add():
-        uebo = Todo(name='uebo')
-        db.session.add(uebo)
+        return render_template('add.html')
+        # uebo = Todo(name='uebo')
+        # db.session.add(uebo)
+        # db.session.commit()
+        # return 'ueboを増やしました。'
+
+    @app.route('/add', methods=['POST'])
+    def postAdd():
+        userData = request.form.get('userName') # hoge
+        userName = Todo(name=userData)
+        db.session.add(userName)
         db.session.commit()
-        return 'ueboを増やしました。'
+        return '新規ユーザーを追加しました。'
 
     @app.route('/delete')
     def delete():
-        id = 1
-        record_to_delete = db.session.query(Todo).filter_by(id=id).first()
+        users = Todo.query.all()
+        return render_template('delete.html',users=users)
+
+    @app.route('/delete', methods=['POST'])
+    def postDelete():
+        userData = request.form.get('id') 
+        record_to_delete = db.session.query(Todo).filter_by(id=userData).first()
 
         if record_to_delete is not None:
-            db.session.delete(perecord_to_deleteter)
+            db.session.delete(record_to_delete)
             db.session.commit()
-            return 'ID:1を削除しました'
+            return f'ID:{userData}を削除しました'
         else:
             return '該当するデータはありません'
 
