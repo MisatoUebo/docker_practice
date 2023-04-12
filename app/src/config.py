ecaf257b7655d0331a61ce830ef8a1809c1ee9b5
MisatoUebo <misato.uebo@tongull.co.jp>
2023-04-12 13:11:12 +0900

    flash機能を使用するためのシークレットキー追加

--- app/src/config.py
+++ app/src/config.py
@@ -17,5 +17,7 @@ class SystemConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
+    SECRET_KEY = b'\x98\xf5H\xb0\x90\xef\x8d\xc6\xcb\x12\x92\x19\x90or\xeb\xe0\x85F\xba$\x02\xd8.'
+
 
 Config = SystemConfig
