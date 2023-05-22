4e56f73face8249c7f59080f9b53cd4b0885a1b0
Misato Uebo <misato.uebo@tongullman.co.jp>
2023-05-22 11:39:01 +0900

    画像アップロード時の処理切り替えれるように修正 (途中)

--- app/src/app.py
+++ app/src/app.py
@@ -3,11 +3,20 @@ from flask import Flask,render_template,redirect,flash
 from database import init_db,db
 from models import Todo
 from flask import request
+# ファイル名をチェックする関数
+from werkzeug.utils import secure_filename
 import datetime
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+
+    # 画像のアップロード先のディレクトリ
+    UPLOAD_FOLDER = './static/image'
+    # アップロードされる拡張子の制限
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     
     init_db(app)
 
@@ -19,13 +28,38 @@ def create_app():
 
     @app.route('/add', methods=["GET","POST"])
     def add():
+        # "GET"の時
         if request.method == "GET":
             return render_template("add.html",pageTitle="ブログ記事作成")
+        # "POST"の時
         else:
-            title = request.form.get('title') # hoge
-            body = request.form.get('body') # hoge
-            userName = Todo(title=title , body=body)
-            db.session.add(userName)
+            title = request.form.get('title')
+            body = request.form.get('body')
+            # image_Url = request.form.get('imageUrl')
+
+            # ファイルがなかった場合の処理
+            if 'file' not in request.files:
+                userData = Todo(title=title , body=body,image_url="No File")
+
+                db.session.add(userData)
+                db.session.commit()
+                return redirect("/")
+
+            # データの取り出し
+            file = request.files['file']
+
+            if file and allwed_file(file.filename):
+                # 危険な文字を削除（サニタイズ処理）
+                filename = secure_filename(file.filename)
+                # ファイルの保存
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                userData = Todo(title=title , body=body,image_url=filename)
+
+            # else:
+            #     file.save(os.path.join('./static/image', file.filename))
+            #     userData = Todo(title=title , body=body,image_url=file.filename)
+            
+            db.session.add(userData)
             db.session.commit()
             return redirect("/")
 
@@ -72,6 +106,11 @@ def create_app():
         if request.method == "GET":
             return render_template("contents.html",pageTitle="内容",post=post)
     
+    def allwed_file(filename):
+        # .があるかどうかのチェックと、拡張子の確認
+        # OKなら１、だめなら0
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
     return app
 
 app = create_app()
